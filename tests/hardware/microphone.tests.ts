5d84ddd189df8db856df707e576277af9ea40955
Elias Binder <ebinder@unibz.it>
2023-03-30 14:01:39 +0200

    Fix test for microphone input stream test

--- tests/hardware/microphone.tests.ts
+++ tests/hardware/microphone.tests.ts
@@ -1,16 +1,14 @@
 import {getStream, stopStream} from "../../src/hardware/microphone";
+import { setTimeout } from 'timers/promises';
 import fs from 'fs';
 
 test('save 10 seconds of microphone data to file', async () => {
-    jest.setTimeout(11000);
     const stream = getStream();
     const fileWriteStream = fs.createWriteStream('resources/test.wav');
     stream.pipe(fileWriteStream);
-    setTimeout(() => {
-        stream.unpipe(fileWriteStream);
-        fileWriteStream.close();
-        stopStream();
-        expect(fs.existsSync('test.wav')).toBe(true);
-    }, 10000);
-    jest.runAllTimers();
-})
+    const result = await setTimeout(10000, 'resolved')
+    stream.unpipe(fileWriteStream);
+    fileWriteStream.close();
+    stopStream();
+    expect(fs.existsSync('resources/test.wav')).toBe(true);
+}, 13000)
