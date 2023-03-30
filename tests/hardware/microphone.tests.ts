02f62eb274c82f104188b65e32874dc779f2f484
Elias Binder <ebinder@unibz.it>
2023-03-30 17:10:49 +0200

    Implement audio output to play audio files

--- tests/hardware/microphone.tests.ts
+++ tests/hardware/microphone.tests.ts
@@ -1,14 +1,14 @@
-import {getStream, stopStream} from "../../src/hardware/microphone";
+import {getMicrophoneStream, stopMicrophoneStream} from "../../src/hardware/microphone";
 import { setTimeout } from 'timers/promises';
 import fs from 'fs';
 
 test('save 10 seconds of microphone data to file', async () => {
-    const stream = getStream();
+    const stream = getMicrophoneStream();
     const fileWriteStream = fs.createWriteStream('resources/test.wav');
     stream.pipe(fileWriteStream);
     const result = await setTimeout(10000, 'resolved')
     stream.unpipe(fileWriteStream);
     fileWriteStream.close();
-    stopStream();
+    stopMicrophoneStream();
     expect(fs.existsSync('resources/test.wav')).toBe(true);
 }, 13000)
