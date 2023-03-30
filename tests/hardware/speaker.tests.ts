3a570fd26ec7409b4351a66f214ba882fe85d5e7
Elias Binder <ebinder@unibz.it>
2023-03-30 17:25:59 +0200

    Remove unnecessary console log

--- tests/hardware/speaker.tests.ts
+++ tests/hardware/speaker.tests.ts
@@ -2,7 +2,6 @@ import {playAudio, currentAudio} from "../../src/hardware/speaker";
 import { setTimeout } from 'timers/promises';
 
 test('play audio file', async () => {
-    console.log('play audio file');
     playAudio('harvard.wav');
     const result = await setTimeout(10000, 'resolved')
     expect(1).toBe(1)
