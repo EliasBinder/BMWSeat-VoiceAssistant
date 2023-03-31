bffa0657ba6ca9484545f5b0944457edb0b3133b
Elias Binder <ebinder@unibz.it>
2023-03-31 15:07:42 +0200

    Add explanation in comments

--- src/volumeLevelAnalyzer/volumeLevelAnalyzer.ts
+++ src/volumeLevelAnalyzer/volumeLevelAnalyzer.ts
@@ -3,15 +3,20 @@ import {wake} from "../app";
 
 export const analyzeStream = (stream: any) => {
     stream.on('data', (chunk: Buffer) => {
-        let average = 0;
+        //Construct array of 16-bit integers representing the audio data
         let out: any = [];
         for (let i = 0; i < chunk.length; i += 2) {
             out.push(chunk.readInt16LE(i));
         }
+
+        //Calculate the average volume
+        let average = 0;
         out.forEach((value: number) => {
             average += value;
         });
         average /= out.length;
         console.log('Avg: ', average);
+
+        //TODO: compare average to threshold
     });
 }
