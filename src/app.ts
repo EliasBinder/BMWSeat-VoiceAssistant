87f50c6d893eceaf8d18139b03dcbd150731c98d
Elias Binder <ebinder@unibz.it>
2023-05-11 11:34:47 +0200

    Fix app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import seatController from "./seatAPI/seatController";
 import {getMicrophoneStream, stopMicrophoneStream} from "./hardware/microphone";
 import {startRestAPI} from "./restapi/httpServer";
 import {sendStreamData} from "./restapi/api/apiRouter";
-import {transcribeMicrophone} from "./speech-to-text/speech_to_text";
+import {stopTranscriptionMicrophone, transcribeMicrophone} from "./speech-to-text/speech_to_text";
 
 //Setup Rest API
 startRestAPI();
@@ -14,25 +14,29 @@ startRestAPI();
 console.log('🎤 Starting volume level analyzer...');
 
 //seatController.makeDummyRequest();
+analyzeStream(() => {
+    console.log('🎤 System is listening...');
+}, async () => {
+    console.log('🎤 System is not listening...');
+    const text = await stopTranscriptionMicrophone();
+    interpretCommand(text);
+});
+
 
 //When wake word is detected or button is pressed: invoke this function
 export const wake = async () => {
-    analyzeStream(() => {
-        console.log('🎤 System is listening...');
-    }, () => {
-        console.log('🎤 System is not listening...');
-        stopMicrophoneStream();
-    });
     console.log('🚀 System is awake!');
-    const transcript = await transcribeMicrophone();
-    console.log('Transcript: ', transcript);
+    transcribeMicrophone();
+}
+
+const interpretCommand = async (command: string) => {
     try {
-        const gptResponse = await interpretMessage(transcript);
-        gptResponse.forEach((value: any) => {
-            console.log('GPT Response JSON: ', JSON.stringify(value));
-            sendStreamData(value);
+        const gptResponse = await interpretMessage(command);
+        gptResponse.forEach((value: string) => {
+            console.log('GPT Response JSON: ', JSON.stringify(gptResponse));
         });
         //TODO: process json -> move motor & play audio
+        sendStreamData(gptResponse);
     } catch (e) {
         playAudio('error.mp3');
         console.log('❌ GPT Response JSON: ', e)
