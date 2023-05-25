7204352679842fef83a09ae0835a958e2428a8fb
Elias Binder <ebinder@unibz.it>
2023-05-25 11:35:33 +0200

    Refactor dov test

--- tests/direction-of-voice/dov.tests.ts
+++ tests/direction-of-voice/dov.tests.ts
@@ -1,16 +1,12 @@
 import { Endpoint, InEndpoint } from "usb";
-import {fetchMicrophoneInterrupts} from "../../src/direction-of-voice/directionOfVoice";
+import {fetchMicrophoneInterrupts, stopFetchMicrophoneInterrupts} from "../../src/direction-of-voice/directionOfVoice";
 import {setTimeout} from "timers/promises";
 
 test('log usb interrupts for microphone', async () => {
-    //@ts-ignore
-    const [endpoint, deviceInterface, isKernelDriverActive] = fetchMicrophoneInterrupts();
-    const result = await setTimeout(30000, 'resolved')
-    endpoint?.stopPoll()
-
-    deviceInterface.release(() => {
-        if (isKernelDriverActive) {
-            deviceInterface.attachKernelDriver();
-        }
-    })
+    fetchMicrophoneInterrupts();
+    await setTimeout(10000, 'resolved');
+    const result = await stopFetchMicrophoneInterrupts();
+    expect(result).toBeGreaterThan(-1);
+    expect(result).toBeLessThan(1);
+    console.log(result);
 }, 33000);
