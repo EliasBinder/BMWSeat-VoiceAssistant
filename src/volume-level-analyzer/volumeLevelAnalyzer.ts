cd484915318b0130e5575d81cb8ac3b942b94912
Elias Binder <ebinder@unibz.it>
2023-06-01 14:51:28 +0200

    Fix volumeLevelAnalyzer

--- src/volume-level-analyzer/volumeLevelAnalyzer.ts
+++ src/volume-level-analyzer/volumeLevelAnalyzer.ts
@@ -1,44 +1,44 @@
 import config from "../../config.json";
 import {wake} from "../app";
 import {getMicrophoneStream} from "../hardware/microphone";
+import {setTimeout as asyncSetTimeout} from "timers/promises";
 
 
 let enableSent = false;
 let disableSent = true;
 let timeout: any = null;
 
-export const analyzeStream = (onFinish: Function) => {
+export const analyzeStream = async (onFinish: Function) => {
     //wait 1 second before starting to analyze the stream
-    setTimeout(() => {
-        const stream = getMicrophoneStream();
-        stream.on('data', (chunk: Buffer) => {
-            console.log('Receiving data from microphone')
-            //Construct array of 16-bit integers representing the audio data
-            let out: any = [];
-            for (let i = 0; i < chunk.length; i += 2) {
-                out.push(Math.abs(chunk.readInt16LE(i)));
-            }
+    await asyncSetTimeout(400, 'resolved');
+    const stream = getMicrophoneStream();
+    stream.on('data', (chunk: Buffer) => {
+        console.log('Receiving data from microphone')
+        //Construct array of 16-bit integers representing the audio data
+        let out: any = [];
+        for (let i = 0; i < chunk.length; i += 2) {
+            out.push(Math.abs(chunk.readInt16LE(i)));
+        }
 
-            //Calculate the average volume
-            let average = 0;
-            out.forEach((value: number) => {
-                average += value;
-            });
-            average /= out.length;
-
-            if (average > config.volumeThreshold) {
-                if (timeout != null) {
-                    //console.log("[VolumeLevelAnalyzer] Timeout is not null");
-                    clearTimeout(timeout);
-                    timeout = null;
-                }
-            }else {
-                if (timeout == null) {
-                    timeout = setTimeout(() => {
-                        onFinish();
-                    }, 1000);
-                }
-            }
+        //Calculate the average volume
+        let average = 0;
+        out.forEach((value: number) => {
+            average += value;
         });
-    }, 400);
+        average /= out.length;
+
+        if (average > config.volumeThreshold) {
+            if (timeout != null) {
+                //console.log("[VolumeLevelAnalyzer] Timeout is not null");
+                clearTimeout(timeout);
+                timeout = null;
+            }
+        } else {
+            if (timeout == null) {
+                timeout = setTimeout(() => {
+                    onFinish();
+                }, 1000);
+            }
+        }
+    });
 }
