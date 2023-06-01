bc634b850edf391023371e1680f6184f1053bd4f
leok <leo.kerschbaumerlk@gmail.com>
2023-06-01 09:29:43 +0200

    changed interpreter to work with new prompts and use davinci-003 model with the correct parameters

--- src/interpreter/gptInterpreter.ts
+++ src/interpreter/gptInterpreter.ts
@@ -5,14 +5,26 @@ import {extractJson} from "./jsonExtractor";
 
 export async function interpretMessage(userInput: string){
     const prompt = getPromptBefore() + userInput + getPromptAfter();
-    const response = await openAI.openai.createChatCompletion({
+    /*const response = await openAI.openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         temperature: 0,
         messages: [
             {role: "user", content: prompt},
         ],
+    });*/
+    const response = await openAI.openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        temperature: 0,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
     });
-    const fullGPTResponse = response.data.choices[0].message.content;
+
+    console.log("response from openai: " + response.data.choices[0].text);
+    //const fullGPTResponse = response.data.choices[0].message.content;
+    const fullGPTResponse = response.data.choices[0].text;
 
     const gptResponse = extractJson(fullGPTResponse);
     let gptResponseJSON: any[] = [];
--- tests/interpreter/interpreter.tests.ts
+++ tests/interpreter/interpreter.tests.ts
@@ -2,9 +2,9 @@ import { interpretMessage } from "../../src/interpreter/gptInterpreter";
 
 test('give a response/completion to a prompt', async () => {
     jest.setTimeout(1000);
-    const resp = await interpretMessage('Stelle meinen Sitz ganz nach vorne.');
+    const resp = await interpretMessage('Die Pedale sind ein bisschen zu weit weg');
     expect(resp[0]).not.toBe(null);
     console.log(resp);
-    expect(JSON.stringify(resp[0])).toBe('{"message":"Seat forward","value":1}');
+    expect(JSON.stringify(resp[0])).toBe('{"message":"Seat forward","value":"low"}');
     jest.runAllTimers();
 }, 10000)
