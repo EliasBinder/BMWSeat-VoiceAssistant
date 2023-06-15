9a5d8babf40960fb4abdfd8f5d0da07c91d45030
Elias Binder <ebinder@unibz.it>
2023-06-15 11:48:06 +0200

    Add mic test

--- tests/hardware/microphone.tests.ts
+++ tests/hardware/microphone.tests.ts
@@ -1,4 +1,9 @@
-import {recordMicrophoneStream, stopMicrophoneStream} from "../../src/hardware/microphone";
+import {
+    getMicrophoneStream,
+    recordMicrophoneStream,
+    startMicrophoneStream,
+    stopMicrophoneStream
+} from "../../src/hardware/microphone";
 import { setTimeout } from 'timers/promises';
 import fs from 'fs';
 
@@ -8,3 +13,14 @@ test('save 10 seconds of microphone data to file', async () => {
     stopMicrophoneStream('test');
     expect(fs.existsSync('resources/transcription.wav')).toBe(true);
 }, 13000)
+
+test('Get live microphone data', async () => {
+    startMicrophoneStream('test');
+    // @ts-ignore
+    getMicrophoneStream('test')!.stdout.on('data', (chunk: Buffer) => {
+        console.log(chunk);
+    });
+    const result = await setTimeout(10000, 'resolved')
+    stopMicrophoneStream('test');
+    expect(1).toBe(1);
+}, 13000);
