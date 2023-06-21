8a28802d5428d53ba53b8cb618285c1a04a9201a
Elias Binder <ebinder@unibz.it>
2023-06-21 15:24:59 +0200

    Continue fixing prompt

--- src/interpreter/gptInterpreter.ts
+++ src/interpreter/gptInterpreter.ts
@@ -4,7 +4,7 @@ export async function interpretMessage(userInput: string){
     const response = await openAI.openai.createChatCompletion({
         model: "gpt-3.5-turbo-0613",
         messages: [
-            {role: "system", content: "You are a helpful assistant that moves a car seat"},
+            {role: "system", content: "You are a helpful assistant that can move a car seat. You can move the seat forward, backward, up, down, and adjust the backrest. The user may not express his request directly, but instead may express the current circumstances which he is not happy with."},
             {role: "user", content: userInput},
         ],
         functions: [
--- tests/interpreter/interpreter.tests.ts
+++ tests/interpreter/interpreter.tests.ts
@@ -2,14 +2,14 @@ import { interpretMessage } from "../../src/interpreter/gptInterpreter";
 
 test('give a response/completion to a prompt', async () => {
     jest.setTimeout(1000);
-    const resp = await interpretMessage('Ich bin müde!');
+    const resp = await interpretMessage('I cannot reach the pedals!');
     console.log(resp);
     jest.runAllTimers();
 }, 10000)
 
 test('give a response/completion to a prompt', async () => {
     jest.setTimeout(1000);
-    const resp = await interpretMessage('Das Wetter ist schön heute');
+    const resp = await interpretMessage('Seat forward');
     console.log(resp);
     jest.runAllTimers();
 }, 10000)
