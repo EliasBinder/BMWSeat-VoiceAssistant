4f7fff4fb5c8c548883919ef017e81fd768cb1e0
Elias Binder <ebinder@unibz.it>
2023-07-07 09:37:51 +0200

    Fix api call unit test

--- tests/seatapi/seatapi.tests.ts
+++ tests/seatapi/seatapi.tests.ts
@@ -1,15 +1,10 @@
 import {moveHorizontal} from "../../src/seat-API/seatController";
 
-test('Move seat vertical', () => {
-    moveHorizontal('DS', 50)
+test('Move seat vertical', async () => {
+    await moveHorizontal('DS', 50)
         .then((response) => {
             console.log('Response: ');
             console.log(JSON.stringify(response));
-            expect(response).toBe('OK');
-        })
-        .catch((e) => {
-            console.log('Error: ');
-            console.log(JSON.stringify(e));
-            expect(e).toBe('OK');
+            expect(response.Output.substring(0, 47)).toBe('OK-STATUS#BMWCS-HYP1: #0xC8-CONFIRMED DS#01#p50');
         });
 })
