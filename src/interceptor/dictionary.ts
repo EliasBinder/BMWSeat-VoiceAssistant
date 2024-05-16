afa90d01d157b4703eb547093061f63d009b9077
EliasBinder <ebinder@unibz.it>
2024-05-17 00:37:51 +0200

    Debug similarity

--- src/interceptor/dictionary.ts
+++ src/interceptor/dictionary.ts
@@ -1,26 +1,27 @@
 import { calculateSimilarity } from "./similarity";
 
+const unitAbbreviations = ["cm", "mm", "°", "%"];
 const commandDictionary = {
     de: {
         triggerword: ["hyper"],
         domain: ["sitz", "lehne"],
         commandname: ["bewegen"],
         direction: ["vorwärts", "rückwärts", "hoch", "runter"],
-        unit: ["zentimeter, millimeter", "grad", "prozent", "ticks"],
+        unit: ["zentimeter", "millimeter", "grad", "prozent", "ticks", ...unitAbbreviations],
     },
     it: {
         triggerword: ["hyper"],
         domain: ["sedile", "schienale"],
         commandname: ["sposta"],
         direction: ["avanti", "indietro", "su", "giù"],
-        unit: ["centimetri, millimetri", "grado", "percentuale", "ticks"],
+        unit: ["centimetri", "millimetri", "grado", "percentuale", "ticks", ...unitAbbreviations],
     },
     en: {
         triggerword: ["hyper"],
         domain: ["seat", "backrest"],
         commandname: ["move"],
         direction: ["forward", "backward", "up", "down"],
-        unit: ["centimeters, millimeters", "degree", "percent", "ticks"],
+        unit: ["centimeters", "millimeters", "degree", "percent", "ticks", ...unitAbbreviations],
     }
 } as any;
 
@@ -33,6 +34,7 @@ export const getTranslation = (key: string, language: string, section: string): 
         let mostLikelyPercentage = 0;
         commandDictionary[language][section].forEach((element: string) => {
             let similarity = calculateSimilarity(element, key);
+            console.log("Similarity between ", element, " and ", key, " is ", similarity);
             if (similarity > mostLikelyPercentage) {
                 mostLikelyPercentage = similarity;
                 mostLikely = element;
